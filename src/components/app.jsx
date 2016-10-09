ec9bba08e56982d983a2b9f7bb466687dfbe912d
Jonathan Ohayon <me@jonathano.com>
2016-10-09 14:34:14 +0300

    Added a simple Redux store {{needs checking}}

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import { AppBar, FloatingActionButton } from 'material-ui';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 
+import counter from '../stores/points.js';
 import '../style/style.scss';
 
 export default class Application extends Component {
+  constructor () {
+    super();
+    this.state = { count: counter.getState() };
+  }
+
+  onFabClick () {
+    counter.dispatch({ type: 'INCREMENT' });
+  }
+
   render () {
+    const self = this;
+    counter.subscribe(() => self.setState({ count: counter.getState() }));
     return (
       <div>
         <AppBar
@@ -15,8 +27,9 @@ export default class Application extends Component {
         <div className="container">
           <h1>Ayyy lmao</h1>
           <h2>Replace this element in src/components/app.jsx with your actual content!</h2>
+          <h2>Counter: {this.state.count}</h2>
         </div>
-        <FloatingActionButton className="fab">
+        <FloatingActionButton className="fab" onClick={this.onFabClick}>
           <ContentAdd />
         </FloatingActionButton>
       </div>
